7bc22a6e5c6e43b858272d17b4322b5043b22589
Hannan <najma.mahat@student.moringaschool.com>
2023-05-13 17:23:46 +0300

    Completed nested resource rounting

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,43 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = find_item
+    render json: item, include: :user
+  end
+
+  def create
+    user = find_user
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:user_id, :name, :description, :price)
+  end
+
+  def find_item
+    Item.find(params[:id])
+  end
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def not_found(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def show
-    user = User.find_by(id: params[:id])
+    user = find_user
     render json: user, include: :items
+  end
+
+  private
+
+  def find_user
+    User.find(params[:id])
+  end
+
+  def not_found
+    render json: { error: "User not found" }, status: :not_found
   end
 
 end
